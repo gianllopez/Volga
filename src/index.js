983c5040b53d687433cb3565e3079bfec3533bd2
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-10-12 20:10:32 -0500

    prueba del nuevo formlayout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LogIn from './pages/LogIn';
+import LogUp from './pages/LogUp';
 import './index.css';
 
 ReactDOM.render(
-  <LogIn/>,
+  <LogUp/>,
   document.getElementById('root')
 );
