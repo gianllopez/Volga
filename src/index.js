455556ef999da3f607be7f4d327056c31332facc
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-10-12 21:47:16 -0500

    shoptags listo, bueno creo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LogUp from './pages/LogUp';
+import ShopTags from './pages/ShopTags';
 import './index.css';
 
-ReactDOM.render(<LogUp />, document.getElementById('root'));
+ReactDOM.render(<ShopTags/>, document.getElementById('root'));
---
+++ src/pages/ShopTags.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import shoptagsHero from '../assets/ShopTags/shop-tags-hero.svg';
+import './styles/ShopTags.css';
+
+class ShopTags extends Component {
+    render() {
+        return (
+            <div id='shop-tags-wrapper'>
+                <div id='header'>
+                    <h1>¿Con qué etiquetas<br/>relacionarías tu tienda?</h1>
+                    <h2>Selecciona 5-10</h2>
+                    <img src={shoptagsHero} alt="shop-tags-hero"/>
+                </div>                
+                <input type='text' placeholder='Buscar etiquetas' id='search-filter'/>
+                <h3>Etiquetas seleccionadas</h3>
+                <section id='selected-tags'/>   
+                <button>Continuar</button>                 
+            </div>
+        );
+    };
+};
+
+export default ShopTags;
