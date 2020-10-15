3d75097015f3fa0453892337d648508429c9def6
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-10-15 14:36:46 -0500

    punto pa empezar a usar la rest

--- src/pages/LogUp.js
+++ src/pages/LogUp.js
@@ -1,28 +1,66 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import FormLayout from '../components/global/FormLayout';
 import logupHero from '../assets/LogUp/logup-hero.svg';
 import './styles/LogUp.css';
 
 class LogUp extends Component {
+    constructor(props) {}
+
     render() {
         return (
-            <FormLayout 
+            <FormLayout
                 headerImg={logupHero}
-                headerTitle={<h2>Rellena lo datos para<br/>registrar tu tienda con<br/>nosotros</h2>}
+                headerTitle={
+                    <h2>
+                        Rellena lo datos para
+                        <br />
+                        registrar tu tienda con
+                        <br />
+                        nosotros
+                    </h2>
+                }
                 formEntries={
                     <Fragment>
-                        <input type='text' name='shop' placeholder='Tienda' required/>
-                        <input type='email' name='email' placeholder='Correo' required/>
-                        <input type='password' name='password' placeholder='Contraseña' required/>                    
-                        <input type='text' name='address' placeholder='Dirección'/>
-                        <textarea name='description' placeholder='Descripción'/>
+                        <input
+                            type='text'
+                            name='shop'
+                            placeholder='Tienda'
+                            required
+                        />
+                        <input
+                            type='email'
+                            name='email'
+                            placeholder='Correo'
+                            required
+                        />
+                        <input
+                            type='password'
+                            name='password'
+                            placeholder='Contraseña'
+                            required
+                        />
+                        <input
+                            type='text'
+                            name='address'
+                            placeholder='Dirección'
+                            required
+                        />
+                        <textarea
+                            name='description'
+                            placeholder='Descripción'
+                            required
+                        />
                         <button>Continuar</button>
-                        <p>¿Ya tienes cuenta?<br/>Ingresa</p>                    
+                        <p>
+                            ¿Ya tienes cuenta?
+                            <br />
+                            Ingresa
+                        </p>
                     </Fragment>
                 }
             />
         );
-    };
-};
+    }
+}
 
 export default LogUp;
--- src/pages/SocialNets.js
+++ src/pages/SocialNets.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import SocialNetsInput from '../components/SocialNets/SocialNetInput';
 import './styles/SocialNets.css';
 
