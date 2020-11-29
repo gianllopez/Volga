1f7caa790a8e57e5b9808af6a0ce4c4fb280daeb
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-11-28 21:28:01 -0500

    ready el error displayer del logup, a probar otra cosa

---
+++ src/components/log-forms/BlankErrorDisplayer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/BlankErrorDisplayer.css';
+
+const BlankErrorDisplayer = _ => (
+    <div id='blank-error-displayer'>
+        <p>Debes rellenar todos los datos del formulario.</p>            
+    </div>
+)
+
+export default BlankErrorDisplayer;
--- src/pages/Logup.js
+++ src/pages/Logup.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 import FormLayout from '../components/global/FormLayout';
-import Loader from '../components/global/Loader';
 import Input from '../components/log-forms/Input';
+import Loader from '../components/global/Loader';
+import BlankErrorDisplayer from '../components/log-forms/BlankErrorDisplayer.js'
 
 import logupHero from '../assets/Logup/logup-hero.svg';
 import './styles/Logup.css';
@@ -33,34 +35,45 @@ class Logup extends Component {
 
     handleSubmit = event => {        
         
-        event.preventDefault();        
+        event.preventDefault();  
         
-        this.setState({ loading: true });
-
-        fetch('https://volga-rest.herokuapp.com/logup/', {
-            method: 'post',
-            headers: {
-                'Authorization': 'Token 007071b38dae7a95425fa0eaf65db37566adfe41',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state.data)
-        })
-            .then(response => {
+        let validationError;
+        
+        let fieldsData = Object.values(this.state.data);
+        
+        for (let field of fieldsData) {
+            if (!field) {
+                validationError = true
+                break
+            }
+        };
+        
+        if (!validationError) {        
+            this.setState({ loading: true });
+            fetch('https://volga-rest.herokuapp.com/logup/', {
+                method: 'post',
+                headers: {
+                    'Authorization': 'Token 629abde4ff2ffecb8c5448baa8c58454e19a2259',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state.data)
+            }).then(response => {
+                this.setState({ loading: false })
                 if (response.ok) {
-                    this.setState({ loading: false }, _ => this.props.history.push('/login'));                    
-                } else {                    
-                    return response.json()
+                    this.props.history.push('/login')
+                } else {
+                    return response.json();
                 }
+            }).then(json => {
+                this.setState({ errors_messages: json })    
             })
-                .then(json => {
-                    if (this._isMounted) {
-                        this.setState({
-                            loading: false,
-                            errors_messages: json                        
-                        })
-                    }
-                })
-            .catch(error => console.error(error))
+        } else {
+            this.setState({ blank_error: true })
+            setInterval(_ => {
+                document.getElementById('blank-error-displayer').style.transform = 'initial'
+            }, 1);
+        }
+        
     };
 
     render() {
@@ -71,6 +84,12 @@ class Logup extends Component {
                 headerTitle={<h2>Rellena lo datos para<br/>registrar tu tienda con<br/>nosotros</h2>}
                 formEntries={
                     <Fragment>
+                        {this.state.blank_error && (
+                            ReactDOM.createPortal(
+                                <BlankErrorDisplayer/>, 
+                                document.getElementById('root')
+                            )            
+                        )}
                         <Input
                             label='Tienda'
                             name='shop'
@@ -127,3 +146,21 @@ class Logup extends Component {
 }
 
 export default Logup;
+
+// fetch('https://volga-rest.herokuapp/logup/', {
+//     method: 'post',
+//     headers: {
+//         'Authorization': 'Token 629abde4ff2ffecb8c5448baa8c58454e19a2259',
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(this.state.data)
+// }).then(response => {
+//     this.setState({ loading: false })
+//     if (response.ok) {
+//         this.props.history.push('/login')
+//     } else {
+//         return response.json();
+//     }
+// }).then(json => {
+//     this.setState({ errors_messages: json });
+// })
