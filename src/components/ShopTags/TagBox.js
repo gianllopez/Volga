507b0f957712ce15eb74ece1ab5e69881849c7ae
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-01 20:47:06 -0500

    añadido los icons de shop tag

---
+++ src/components/ShopTags/TagBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import laptop_icon from '../../assets/ShopTags/laptop.svg';
+
+class TagBox extends Component {
+  
+   render() {
+      return (
+         false
+      );
+   };
+
+};
+
+export default TagBox;
