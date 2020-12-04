4d34663d2720e48682dda7cbfb6670dcaf377c38
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-03 23:37:48 -0500

    hechas las bases de las page del post product

---
+++ src/pages/PostProduct.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/PostProduct.css';
+
+class PostProduct extends Component {
+   render() {
+      return (
+         false
+      );
+   };
+};
+
+export default PostProduct;
