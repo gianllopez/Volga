9aace04d3c4ba5b06d92f69a8fc2b465fe66808e
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-06 02:38:12 -0500

    Logup: terminado el header

--- src/pages/Logup.js
+++ src/pages/Logup.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import loguphero from '../assets/Logup/logup-hero.svg';
 import './styles/Logup.css';
 
 class Logup extends Component {
 	render() {
-		return false;
+		return (
+			<form id="logup-form">
+				<div id="logup-header">
+					<img src={loguphero} alt="logup-hero"/>
+					<h2>
+						Registra tu<br/>
+						tienda con<br/>
+						nosotros
+					</h2>
+				</div>
+			</form>
+		);
 	};
 };
 
