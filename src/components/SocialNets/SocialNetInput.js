bc020d2e35d711a4c4e87f290e973ce43521db7e
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-09 23:23:38 -0500

    Social Nets: resumido el componente de social nets input pero hay un error en las anims

--- src/components/SocialNets/SocialNetInput.js
+++ src/components/SocialNets/SocialNetInput.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import animationSetter from './local.utils';
 import checkIcon from '../../assets/SocialNets/check-icon.svg';
 import {snicons} from '../../utils/assets-exports';
 import './styles/SocialNetInput.css';
@@ -31,47 +32,14 @@ class SocialNetInput extends Component {
          </div>
       );
    }
-
+   
    componentDidMount() {
-
-      const colors = {
-         instagram: '#c32aa3',
-         facebook: '#1877f2',
-         whatsapp: '#25d366',
-         twitter: '#1da1f2',
-         pinterest: '#c8232c',
-      };
-
-      Object.entries(document.getElementsByClassName('social-network')).map(
-         (socialNet) => {
-
-            socialNet[1].addEventListener('click', function () {
-               let header = this.querySelector('h1');
-               let dataInput = this.querySelector('#data-input');
-               let input = this.querySelector('input');
-               let checkBtn = this.querySelector('#data-input > img');
-               let isFocused = input === document.activeElement;
-
-               if (!isFocused) {
-                  header.classList.toggle('header-anim');
-                  dataInput.classList.toggle('data-input-anim');
-               }
-
-               checkBtn.addEventListener('click', function (event) {
-                  event.stopPropagation();
-                  let name = input.placeholder;
-                  header.classList.remove('header-anim');
-                  dataInput.classList.remove('data-input-anim');
-                  let value = input.value;
-                  header.innerText = value ? value : name;
-                  header.style.color = value
-                     ? colors[name.toLowerCase()]
-                     : 'initial';
-               });
-            });
-         },
+      const socialnetworks = document.getElementsByClassName('social-network');
+      Object.entries(socialnetworks).map(
+         socialnet => animationSetter(socialnet)
       );
-   }
-}
+   };
+
+};
 
 export default SocialNetInput;
