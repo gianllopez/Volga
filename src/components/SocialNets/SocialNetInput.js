aea06bda4ba2d19bbcd29201642cbf051b673973
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-09 20:33:23 -0500

    Social Nets: terminado pero el responsive puede ser mejor, a probar

--- src/components/SocialNets/SocialNetInput.js
+++ src/components/SocialNets/SocialNetInput.js
@@ -1,98 +1,77 @@
 import React, { Component } from 'react';
-
-import instagramIcon from '../../assets/SocialNets/instagram-icon.svg';
-import facebookIcon from '../../assets/SocialNets/facebook-icon.svg';
-import whatsappIcon from '../../assets/SocialNets/whatsapp-icon.svg';
-import twitterIcon from '../../assets/SocialNets/twitter-icon.svg';
-import pinterestIcon from '../../assets/SocialNets/pinterest-icon.svg';
 import checkIcon from '../../assets/SocialNets/check-icon.svg';
-
+import {snicons} from '../../utils/assets-exports';
 import './styles/SocialNetInput.css';
 
 class SocialNetInput extends Component {
-    
-    icons = {
-        instagram: instagramIcon,
-        facebook: facebookIcon,
-        whatsapp: whatsappIcon,
-        twitter: twitterIcon,
-        pinterest: pinterestIcon
-    };
 
-    name = this.props.snName;
+   name = this.props.name;
 
-    Name = (str = this.name) => str.charAt(0).toUpperCase() + str.substring(1);
+   Name = this.name.charAt(0).toUpperCase() + this.name.substring(1);
 
-    isError = _ => Object.keys(this.props.errorsObject || {}).includes(this.name)
-
-    render() {
-        return (
-            <div className='social-network'>
-                <figure id='logo-wrapper'>
-                    <img src={this.icons[this.name]} alt={`${this.name}-icon`}/>
-                </figure>
-                <div id='div-line'/>
-                <div id='data-input'>
-                    <input
-                        type='text'
-                        maxLength='30'
-                        placeholder={this.Name()}
-                        name={this.name}
-                        autoComplete='off'
-                        onChange={this.props.onChange}
-                    />
-                    <img src={checkIcon} alt='check-icon'/>
-                </div>
-                <h1>{this.Name()}</h1>
-                {this.isError() && (
-                    <span id='sn-error-span'>
-                        {this.props.errorsObject[this.name]}
-                    </span>
-                )}
+   render() {
+      return (
+         <div className='social-network'>
+            <figure id='logo-wrapper'>
+               <img src={snicons[this.name]} alt={`${this.name}-icon`}/>
+            </figure>
+            <div id='div-line'/>
+            <div id='data-input'>
+               <input
+                  type='text'
+                  maxLength='30'
+                  placeholder={this.Name}
+                  name={this.name}
+                  autoComplete='off'
+                  onChange={this.props.onChange}
+               />
+               <img src={checkIcon} alt='check-icon'/>
             </div>
-        );
-    }
+            <h1>{this.Name}</h1>
+         </div>
+      );
+   }
 
-    componentDidMount() {
-        
-        const colors = {
-            instagram: '#c32aa3',
-            facebook: '#1877f2',
-            whatsapp: '#25d366',
-            twitter: '#1da1f2',
-            pinterest: '#c8232c',
-        };
+   componentDidMount() {
 
-        Object.entries(document.getElementsByClassName('social-network')).map(
-            (socialNet) => {
+      const colors = {
+         instagram: '#c32aa3',
+         facebook: '#1877f2',
+         whatsapp: '#25d366',
+         twitter: '#1da1f2',
+         pinterest: '#c8232c',
+      };
 
-                socialNet[1].addEventListener('click', function () {
-                    let header = this.querySelector('h1');
-                    let dataInput = this.querySelector('#data-input');
-                    let input = this.querySelector('input');
-                    let checkBtn = this.querySelector('#data-input > img');
-                    let isFocused = input === document.activeElement;
+      Object.entries(document.getElementsByClassName('social-network')).map(
+         (socialNet) => {
 
-                    if (!isFocused) {
-                        header.classList.toggle('header-anim');
-                        dataInput.classList.toggle('data-input-anim');
-                    }
+            socialNet[1].addEventListener('click', function () {
+               let header = this.querySelector('h1');
+               let dataInput = this.querySelector('#data-input');
+               let input = this.querySelector('input');
+               let checkBtn = this.querySelector('#data-input > img');
+               let isFocused = input === document.activeElement;
 
-                    checkBtn.addEventListener('click', function (event) {
-                        event.stopPropagation();
-                        let name = input.placeholder;
-                        header.classList.remove('header-anim');
-                        dataInput.classList.remove('data-input-anim');
-                        let value = input.value;
-                        header.innerText = value ? value : name;
-                        header.style.color = value
-                            ? colors[name.toLowerCase()]
-                            : 'initial';
-                    });
-                });
-            },
-        );
-    }
+               if (!isFocused) {
+                  header.classList.toggle('header-anim');
+                  dataInput.classList.toggle('data-input-anim');
+               }
+
+               checkBtn.addEventListener('click', function (event) {
+                  event.stopPropagation();
+                  let name = input.placeholder;
+                  header.classList.remove('header-anim');
+                  dataInput.classList.remove('data-input-anim');
+                  let value = input.value;
+                  header.innerText = value ? value : name;
+                  header.style.color = value
+                     ? colors[name.toLowerCase()]
+                     : 'initial';
+               });
+            });
+         },
+      );
+   }
 }
 
 export default SocialNetInput;
--- src/pages/SocialNets.js
+++ src/pages/SocialNets.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import SocialNetInput from '../components/SocialNets/SocialNetInput';
 import './styles/SocialNets.css';
 
 class SocialNets extends Component {
 	render() {
-		return false;
+		return (
+			<form id="socialnets-form">
+					<h2>Redes sociales y de<br/>contacto de tu tienda</h2>
+				<div id="socialnets-entries">
+					<SocialNetInput name="instagram"/>
+					<SocialNetInput name="facebook"/>
+					<SocialNetInput name="whatsapp"/>
+					<SocialNetInput name="twitter"/>
+					<SocialNetInput name="pinterest"/>
+				</div>
+				<button>Continuar</button>
+			</form>
+		);
 	};
 };
 
