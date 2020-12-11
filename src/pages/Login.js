76aa545b6a53815d9e38a3a3bdd210eb4bea62ca
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-11 16:10:25 -0500

    Login: UI creo que lista, a ver si resumo code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Input from '../components/common/Input';
+import loginhero from '../assets/Login/login-hero.svg';
 import './styles/Login.css';
 
 class Login extends Component {
 	render() {
-		return false;
+		return (
+			<form id="login-form">
+				<div id="login-header">
+					<img src={loginhero} alt="login-hero"/>
+					<div id="header-text">
+						<h1>Inicia sesión ahora</h1>
+						<p>Así podrás estar al día con tu tienda</p>
+					</div>
+				</div>
+				<div id="login-entries">
+					<Input
+						label="Tienda o Email"
+						name="shoporemail"
+					/>
+					<Input
+						label="Contraseña"
+						name="password"
+						type="password"
+					/>
+					<button>Iniciar</button>
+					<p>
+						¿No has registrado tu tienda?<br/>
+						<a href="">Regístrala</a>
+					</p>
+				</div>
+			</form>
+		);
 	};
 };
 
