333a41be2e4d3ce4d29f651463e1ad282b92c3f0
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-10 19:22:58 -0500

    Social Nets: las validaciones ya se hacen

--- src/pages/SocialNets.js
+++ src/pages/SocialNets.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import SocialNetInput from '../components/SocialNets/SocialNetInput';
 import {blankValidator} from '../utils/validators';
+import swal from '@sweetalert/with-react';
 import './styles/SocialNets.css';
 
 class SocialNets extends Component {
 	
-	state = {};
+	state = {
+		data: {
+			instagram: '', facebook: '', whatsapp: '',
+			twitter: '', pinterest: ''
+		}
+	};
 
 	changeHandler = event => {
 		this.setState({
@@ -15,10 +21,23 @@ class SocialNets extends Component {
 			}
 		});
 	};
+
+	submitHandler = event => {
+		event.preventDefault();
+		this.setState({ loading: true, error: false });
+		const {isValid, errors} = blankValidator(this.state.data);
+		if (isValid) {
+			console.log('MAKE FETCH...')
+		} else {
+			swal({
+				title: 'Alguna redes no se rellenaron'
+			});
+		};
+	};
 	
 	render() {
 		return (
-			<form id="socialnets-form">
+			<form id="socialnets-form" onSubmit={this.submitHandler}>
 					<h2>Redes sociales y de<br/>contacto de tu tienda</h2>
 				<div id="socialnets-entries">
 					<SocialNetInput
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -3,7 +3,7 @@ export function blankValidator(data, alloweds) {
    const formData = Object.entries(data);
    let errors = {};
    for (let x of formData) {
-      if (!x[1] && !alloweds.includes(x[0])) {
+      if (!x[1] && !alloweds || [].includes(x[0])) {
          errors[x[0]] = 'Este campo es requerido.'
          isValid = false
       }
