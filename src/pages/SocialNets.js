7916eb6909ba1ca1135e70a140740c1782e1feb8
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-10 19:15:49 -0500

    Social Nets: agregado el handler de cambios en los inputs

--- src/pages/SocialNets.js
+++ src/pages/SocialNets.js
@@ -1,18 +1,46 @@
 import React, {Component} from 'react';
 import SocialNetInput from '../components/SocialNets/SocialNetInput';
+import {blankValidator} from '../utils/validators';
 import './styles/SocialNets.css';
 
 class SocialNets extends Component {
+	
+	state = {};
+
+	changeHandler = event => {
+		this.setState({
+			data: {
+				...this.state.data,
+				[event.target.name]: event.target.value
+			}
+		});
+	};
+	
 	render() {
 		return (
 			<form id="socialnets-form">
 					<h2>Redes sociales y de<br/>contacto de tu tienda</h2>
 				<div id="socialnets-entries">
-					<SocialNetInput name="instagram"/>
-					<SocialNetInput name="facebook"/>
-					<SocialNetInput name="whatsapp"/>
-					<SocialNetInput name="twitter"/>
-					<SocialNetInput name="pinterest"/>
+					<SocialNetInput
+						name="instagram"
+						onChange={this.changeHandler}
+					/>
+					<SocialNetInput
+						name="facebook"
+						onChange={this.changeHandler}
+					/>
+					<SocialNetInput
+						name="whatsapp"
+						onChange={this.changeHandler}
+					/>
+					<SocialNetInput
+						name="twitter"
+						onChange={this.changeHandler}
+					/>
+					<SocialNetInput
+						name="pinterest"
+						onChange={this.changeHandler}
+					/>
 				</div>
 				<button>Continuar</button>
 			</form>
