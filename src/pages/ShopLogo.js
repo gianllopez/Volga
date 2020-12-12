eb5e96d6c6f7815ecbef8b0d9a82026a2ae74aa0
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-12 14:39:17 -0500

    Shop Logo: adelantado el UI

--- src/pages/ShopLogo.js
+++ src/pages/ShopLogo.js
@@ -1,20 +1,19 @@
 import React, {Component} from 'react';
 import ProgressBar from '../components/ShopLogo/ProgressBar';
 import shoplogohero from '../assets/ShopLogo/shoplogo-hero.svg';
+import './styles/ShopLogo.css';
 
 class ShopLogo extends Component {
    render() {
       return (
-         <form id="shop-logo" encType="multipart/form-data">
-            <div id="logo-header">
-               <img src={shoplogohero} alt="shoplogo-hero"/>
-               <h1>Selecciona el logo de tu tienda</h1>
-               <p>Esto es super importante</p>
-            </div>
-            <div id="logo-uploader">
-               <button>Subir imagen</button>
-               <ProgressBar/>
-            </div>
+         <form id="shoplogo-form" encType="multipart/form-data">
+            <img src={shoplogohero} alt="shoplogo-hero"/>
+            <h1>Selecciona un logo<br/>para tu tienda</h1>
+            <p>Haz que tus clientes la reconozcan</p>
+            <button id="upload-btn">Subir imagen</button>
+            <ProgressBar/>
+            <button>Continuar</button>
+
          </form>
       );
    };
