1b97d30b588f7dce7b836bac766ea6ac2ec97b2a
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-14 23:33:48 -0500

    Shop Logo: ahora si, ready todo, a resumir code

--- src/components/ShopLogo/Uploader.js
+++ src/components/ShopLogo/Uploader.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import uploadicon from '../../assets/ShopLogo/upload-icon.svg';
+import checkicon from '../../assets/ShopLogo/check-icon.svg';
 import './styles/Uploader.css';
 
 class Uploader extends Component {
+
+   state = {
+      success: false
+   }
 
    uploaderTrigger = event => {
       event.preventDefault();
@@ -14,11 +19,9 @@ class Uploader extends Component {
          <div id="uploader-wrapper">            
             <button id="loadlogo-btn">
                <figure>
-                  <img src={uploadicon} alt="upload-icon"/>
+                  <img src={uploadicon} alt="loader-icon"/>
                </figure>
-               <span>
-                  Cargar logo
-               </span>
+               <span>Cargar logo</span>               
             </button>
             <input
                type="file"
@@ -29,6 +32,20 @@ class Uploader extends Component {
             />
          </div>
       );
+   };
+
+   componentDidUpdate() {
+      if (this.state.success) {
+         const loadimg = document.querySelector('#loadlogo-btn figure img');
+         const loadspan = document.querySelector('#loadlogo-btn span')
+         loadimg.style.animation = 'img-animation 1s';
+         loadspan.style.animation = 'span-animation 1s';
+         setTimeout(() => {
+            loadimg.src = checkicon
+            loadspan.innerText = 'Logo cargado'
+            loadspan.classList.add('on-success')
+         }, 500);
+      }
    };
    
 };
