098b43bb5ba7b7ca0f671f27992fc25ca20d85ff
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-16 23:17:45 -0500

    ShopProfile: resumido un poco de code

--- src/pages/components/ShopProfile/ShopStats.js
+++ src/pages/components/ShopProfile/ShopStats.js
@@ -1,37 +1,35 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { statsicons } from '../../../assets/';
-import './ShopStats.css';
+import './styles/ShopStats.css';
 
-function Stat(props) {
-   const {stat, label, value} = props;
-   return (
-      <div className={`stat ${stat}`}>
-         <figure>
-            <img src={statsicons[stat]} alt={`${stat}-icon`}/>
-         </figure>
-         <span>
-            {label}
-            <p>{value}</p>
-         </span>
-      </div>
-   );
-};
-
-class ShopStats extends Component {
+function ShopStats(props) {
    
-   defaultValues = {stars: 0, followers: 0, products: 0}
+   const defaultValues = {stars: 0, followers: 0, products: 0}; 
+   let {stars, followers, products} = props.values || defaultValues;      
    
-   render() {   
-      let {stars, followers, products} = this.props.values || this.defaultValues;   
+   const Stat = (props) => {
+      let {stat, label, value} = props;
       return (
-         <Fragment>         
-            <Stat stat="rating" label="Calificación" value={stars}/>
-            <Stat stat="followers" label="Seguidores" value={followers}/>
-            <Stat stat="products" label="Productos" value={products}/>
-         </Fragment>
+         <div className={`stat ${stat}`}>
+            <figure>
+               <img src={statsicons[stat]} alt={`${stat}-icon`}/>
+            </figure>
+            <span>
+               {label}
+               <p>{value}</p>
+            </span>
+         </div>
       );
    };
-   
+
+   return (
+      <Fragment>         
+         <Stat stat="rating" label="Calificación" value={stars}/>
+         <Stat stat="followers" label="Seguidores" value={followers}/>
+         <Stat stat="products" label="Productos" value={products}/>
+      </Fragment>
+   );
+
 };
 
 export default ShopStats;
