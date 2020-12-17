e8c60f6a44726b78c273c18dc2f3b990ce4a504a
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-17 15:33:26 -0500

    Product Page: parcialmente listo

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -8,14 +8,16 @@ class ProductPage extends Component {
             <figure>
                <img src="https://cdn.shopify.com/s/files/1/0237/2580/1549/products/2_3d9041e2-f964-4885-af28-497fc3678cd1.png?v=1564329267" alt="product-image"/>
             </figure>
-            <h2>Air Yeezy 2 "Red October"</h2>
-            <h4>$500</h4>
-            <p>
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id voluptatibus corporis dolores perspiciatis, nulla soluta nemo, expedita ullam unde porro sint aliquam quo, quia atque?
-            </p>
-            <div id="iproduct-btns">
-               <button>Preguntar</button>
-               <button>Añadir a deseos</button>
+            <div id="productpage-info">
+               <h2>Air Yeezy 2 "Red October"</h2>
+               <h4>$500</h4>
+               <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id voluptatibus corporis dolores perspiciatis, nulla soluta nemo, expedita ullam unde porro sint aliquam quo, quia atque?
+               </p>
+               <div id="iproduct-btns">
+                  <button>Preguntar</button>
+                  <button>Añadir a deseos</button>
+               </div>
             </div>
          </div>
       );
