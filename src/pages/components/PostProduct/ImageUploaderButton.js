35bc6d564b0330c8592fdeab4d8c23ec695b889b
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-19 08:47:31 -0500

    Trigger al click listo

--- src/pages/components/PostProduct/ImageUploaderButton.js
+++ src/pages/components/PostProduct/ImageUploaderButton.js
@@ -4,15 +4,18 @@ import closeicon from '../../../assets/PostProduct/close-img.svg';
 import './styles/ImageUploaderButton.css';
 
 class ImgUploaderButton extends Component {
+   
+   loaderTrigger = () => document.querySelector(`input[name="prodimg${this.props.index}"]`).click()
+   
    render() {
       return (
-         <div className="imguploader-wrapper">
+         <div className="imguploader-wrapper" onClick={this.loaderTrigger}>
             <button className="imgloader-btn" type="button">
                <img src={uploadicon} alt="upload-icon"/>
             </button>
             <input
                type="file"
-               name={`productimage${this.props.index}`}
+               name={`prodimg${this.props.index}`}
                onInput={this.props.inputHandler}
                accept=".png, .jpg, .jpeg"
                hidden               
