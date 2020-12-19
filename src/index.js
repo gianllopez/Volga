78dc2745dbedfbbf2ee28fd210bc6593d87d0203
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-18 20:09:51 -0500

    PostProduct: empezando

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Login from './pages/Login';
 import ShopProfile from './pages/ShopProfile';
 import ProductPage from './pages/ProductPage';
 import Home from './pages/Home';
+import PostProduct from './pages/PostProduct';
 import './index.css';
 
 ReactDOM.render(
@@ -22,6 +23,7 @@ ReactDOM.render(
          <Route exact path='/shops/:shop' component={ShopProfile}/>
          <Route exact path='/:shop/:product' component={ProductPage}/>
          <Route exact path='/' component={Home}/>
+         <Route exact path='/:shop/products/post' component={PostProduct}/>
       </Switch>
    </BrowserRouter>,
    document.getElementById('root')
---
+++ src/pages/PostProduct.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/PostProduct.css';
+
+class PostProduct extends Component {
+   render() {
+      return (
+         <div></div>
+      );
+   };
+};
+
+export default PostProduct;
