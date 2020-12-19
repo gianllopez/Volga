9f3978742697c81fad59d06770fff84bba05a41d
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-18 20:26:05 -0500

    PostProduct: header de la page creo que lista

--- src/pages/PostProduct.js
+++ src/pages/PostProduct.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import prodboxicon from '../assets/PostProduct/product-box.svg';
 import './styles/PostProduct.css';
 
 class PostProduct extends Component {
    render() {
       return (
-         <div></div>
+         <form id="post-product">            
+            <figure>
+               <img src={prodboxicon} alt="product-box-icon"/>
+            </figure>
+            <h2>Postea tu producto</h2>
+            <div id="post-product-form">
+
+            </div>
+         </form>
       );
    };
 };
