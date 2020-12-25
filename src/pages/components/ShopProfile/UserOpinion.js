fc377d175798eafa84c633199adfc2434acefbe3
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-25 14:31:22 -0500

    CSS Grid aplicado al desktop version, a corregir un error

--- src/pages/components/ShopProfile/UserOpinion.js
+++ src/pages/components/ShopProfile/UserOpinion.js
@@ -6,7 +6,7 @@ function UserOpinion(props) {
       <div className="opinion">
          <img src="https://elintra.com.ar/wp-content/uploads/2020/10/LeBron-James-10.jpg" alt="user-photo"/>
          <div className="opinion-content">
-            <h3>LeBron James</h3>
+            <h2>LeBron James</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, sunt.</p>
          </div>
       </div>
