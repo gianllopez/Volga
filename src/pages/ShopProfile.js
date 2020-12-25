3a5968a9187f352d8b196de9538a2b91551f2bd3
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-25 13:08:01 -0500

    Acoplado el navbar a la page del profile pero hay cosas que no me terminan de convencer

--- src/pages/ShopProfile.js
+++ src/pages/ShopProfile.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { ShopStats } from './components/';
-import { ProductCard } from './components/';
-import { UserOpinion } from './components/';
-import { SocialNetLink } from './components/';
+import {
+   ShopStats,
+   ProductCard,
+   UserOpinion,
+   SocialNetLink,
+   NavBar } from './components/';
 import './styles/ShopProfile.css';
 
 class ShopProfile extends Component {
@@ -12,59 +14,52 @@ class ShopProfile extends Component {
    
    render() {
       return (
-         <div id="shop-profile">
-            <section id="profile-header">
-               <figure>
-                  <img src="https://i.pinimg.com/originals/77/b6/6f/77b66fa7469f75773d5eb443056f2f8f.jpg" alt="shop-logo"/>
-               </figure>
-               <div id="shop-info">
-                  <h2>ovotheshop</h2>
-                  <h4>Aubrey Drake Graham</h4>                  
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse!
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse!
-                     Lorem ipsum dolor sit amet consectetur adipisicing e
-                  </p>               
-               </div>
-               <div id="interaction-btns">
-                  <button onClick={this.contactScroll}>Contactar</button>
-                  <button id="follow">Seguir</button>
-               </div>
-            </section>
-            <section id="shop-stats">
-               <ShopStats/>
-            </section>
-            <section id="shop-products">
-               <h3>Aquí puedes encontrar:</h3>
-               <div id="products">
-                  <ProductCard/>
-                  <ProductCard/>
-                  <ProductCard/>
-                  <ProductCard/>
-                  <ProductCard/>
-               </div>               
-            </section>
-            <section id="shop-opinions">
-               <h3>Los clientes opinan:</h3>
-               <div id="opinions">
-                  <UserOpinion/>
-                  <UserOpinion/>
-                  <UserOpinion/>
-                  <UserOpinion/>
-                  <UserOpinion/>
-               </div>
-            </section>
-            <section id="shop-social-networks">
-               <h3>Contacta a *shop*:</h3>
-               <div id="social-networks">
-                  <SocialNetLink name="instagram"/>
-                  <SocialNetLink name="facebook"/>
-                  <SocialNetLink name="whatsapp"/>
-                  <SocialNetLink name="twitter"/>
-                  <SocialNetLink name="pinterest"/>
-               </div>
-            </section>
-         </div>
+         <Fragment>
+            <NavBar/>
+            <div id="shop-profile">
+               <section id="profile-header">
+                  <figure>
+                     <img src="https://i.pinimg.com/originals/77/b6/6f/77b66fa7469f75773d5eb443056f2f8f.jpg" alt="shop-logo"/>
+                  </figure>
+                  <div id="shop-info">
+                     <h2>ovotheshop</h2>
+                     <h4>Aubrey Drake Graham</h4>                  
+                     <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse!
+                        Lorem ipsum dolor sit amet consectetur adipisicing e
+                     </p>               
+                  </div>
+                  <div id="interaction-btns">
+                     <button onClick={this.contactScroll}>Contactar</button>
+                     <button id="follow">Seguir</button>
+                  </div>
+               </section>
+               <section id="shop-stats">
+                  <ShopStats/>
+               </section>
+               <section id="shop-products">
+                  <h3>Aquí puedes encontrar:</h3>
+                  <div id="products">
+                     <ProductCard/>
+                     <ProductCard/>
+                     <ProductCard/>
+                     <ProductCard/>
+                     <ProductCard/>
+                  </div>               
+               </section>
+               <section id="shop-opinions">
+                  <h3>Los clientes opinan:</h3>
+                  <div id="opinions">
+                     <UserOpinion/>
+                     <UserOpinion/>
+                     <UserOpinion/>
+                     <UserOpinion/>
+                     <UserOpinion/>
+                  </div>
+               </section>
+            </div>
+         </Fragment>
       );
    };
 };
