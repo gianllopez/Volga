49c7a284366e74db68f520e5fcc6589f6a39148a
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-27 17:45:16 -0500

    ShopOpinions ready, no me convence mucho el componente de opinion

--- src/index.js
+++ src/index.js
@@ -17,14 +17,14 @@ import './index.css';
 ReactDOM.render(
    <BrowserRouter>
       <Switch>
+         <Route exact path='/' component={Home}/>
          <Route exact path='/logup' component={Logup}/>
          <Route exact path='/:shop/social-networks' component={SocialNets}/>
          <Route exact path='/:shop/tags' component={ShopTags}/>
          <Route exact path='/:shop/logo' component={ShopLogo}/>
          <Route exact path='/login' component={Login}/>
          <Route exact path='/shops/:shop' component={ShopProfile}/>
-         <Route exact path='/:shop/:product' component={ProductPage}/>
-         <Route exact path='/' component={Home}/>
+         <Route exact path='/:shop/products/:product' component={ProductPage}/>
          <Route exact path='/:shop/products/new' component={PostProduct}/>
          <Route exact path='/:shop/opinions' component={ShopOpinions}/>
          <Route exact path='/:shop/opinions/new' component={NewOpinion}/>
--- src/pages/ShopOpinions.js
+++ src/pages/ShopOpinions.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { NavBar, UserOpinion, Footer } from './components/'
 import './styles/ShopOpinions.css';
 
 class ShopOpinions extends Component {
    render() {
       return (
-         <div id="shop-opinions">
-            <h2>Esto es lo que opinan de *shop* sus clientes:</h2>
-            <div id="users-opinions">
-               
+         <Fragment>
+            <NavBar/>
+            <div id="shop-opinions">
+               <h2>Esto es lo que opinan de *shop* sus clientes:</h2>
+               <div id="users-opinions">
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+                  <UserOpinion/>
+               </div>
             </div>
-         </div>
+            <Footer/>
+         </Fragment>
       );
    };
 };
