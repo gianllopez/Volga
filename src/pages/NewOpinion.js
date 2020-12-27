1a9a5b8be36486402401c16ab3e4d4f038536151
Gian Lopez <lopezarizagianlucas@gmail.com>
2020-12-26 20:33:55 -0500

    NewOpinion: header listo en mobile y en desktop

--- src/pages/NewOpinion.js
+++ src/pages/NewOpinion.js
@@ -1,8 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { NavBar } from './components';
+import opsheader from '../assets/NewOpinion/users-opinions.svg';
+import './styles/NewOpinion.css';
 
 class NewOpinion extends Component {
    render() {
-      return (false);
+      return (
+         <Fragment>
+            <NavBar/>
+            <form id="opinion-form">
+               <div id="op-header">
+                  <figure>
+                     <img src={opsheader} alt="opsheader-icon"/>
+                  </figure>
+                  <h2>Opina sobre *shop*</h2>
+                  <p>Déjalos saber que piensas</p>
+               </div>               
+            </form>
+         </Fragment>
+      );
    };
 };
 
