5d3d62066494dea36d79024a2f58074f37b675f1
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-20 20:34:06 -0500

    NewOpinion: a ver si cambio un poco el responsive de PC y a ordenar el code del componente de RatingSelector.

--- src/pages/NewOpinion.js
+++ src/pages/NewOpinion.js
@@ -4,30 +4,41 @@ import opsheader from '../assets/NewOpinion/users-opinions.svg';
 import './styles/NewOpinion.css';
 
 class NewOpinion extends Component {
+
+
+   state = {
+      data: {
+         rating: 5,
+         clientname: '',
+         clientemail: '',
+      },
+      loading: false
+   };
+
    render() {
       return (
          <form id="opinion-form">
             <div id="op-header">
                <figure>
-                  <img src={opsheader} alt="opsheader-icon"/>
+                  <img src={opsheader} alt="opsheader-icon" />
                </figure>
                <h2>Opina sobre *shop*</h2>
-               <p>Déjalos saber que piensas</p>
+               <p>Déjalo saber que piensas</p>
             </div>
-            <RatingSelector/>
+            <RatingSelector />
             <Input
                label="Nombre"
-               name="user-name"
+               name="clientname"
             />
             <Input
                label="Correo"
-               name="user-email"
-               type="email"               
+               name="clientemail"
+               type="email"
             />
             <div id="op-comment">
                <label htmlFor="comment">Comentario: <p>*</p> </label>
-               <textarea name="comment" placeholder="..." maxLength="150"/>
-            </div>        
+               <textarea name="comment" placeholder="..." maxLength="150" />
+            </div>
          </form>
       );
    };
--- src/pages/components/NewOpinion/RatingSelector.js
+++ src/pages/components/NewOpinion/RatingSelector.js
@@ -2,28 +2,49 @@ import React, { Component } from 'react';
 import './RatingSelector.css';
 
 class RatingSelector extends Component {
+
+   state = { currentRating: 5 };
+
+   inputChanger = ({ target }) => {
+      let ratinput = document.querySelector('input[name="rating"]'),
+         { action } = target.dataset;
+      this.value = ratinput.value;
+      ratinput.value = action === 'increment' ?
+         this.value + 0.5 :
+         this.value - 0.5;
+   };
+
    render() {
       return (
          <div id="rating-selector">
-            <div id="rating-view" style={{ backgroundColor: '#6dcc6d' }}>
-               {this.props.currentRating}
+            <button
+               type="button"
+               className="far fa-minus-square"
+               data-action="decrement"
+               onClick={this.inputChanger} />
+            <div id="rating-view">
+               {this.state.currentRating}
             </div>
+            <button
+               type="button"
+               className="far fa-plus-square"
+               data-action="increment"
+               onClick={this.inputChanger} />
             <input
-               type="range"
+               onChange={this.props.onChange}
                name="rating"
+               type="range"
                min="0"
                max="5"
                step="0.5"
-               onChange={this.props.changeHandler}
+               hidden
             />
          </div>
       );
    };
 
    componentDidUpdate() {
-      let rating = parseFloat(this.props.currentRating);
-      let rtview = document.getElementById('rating-view');
-      let bgColor = '';      
+      let rating = parseFloat(this.value), bgColor = '';
       if (rating < 3.0) {
          bgColor = '#FF5722';
       } if (rating >= 3.0 && rating <= 4.0) {
@@ -31,7 +52,8 @@ class RatingSelector extends Component {
       } if (rating > 4.0) {
          bgColor = '#6dcc6d';
       };
-      rtview.style.backgroundColor = bgColor;
+      document.getElementById('rating-view')
+         .style.backgroundColor = bgColor;
    };
 
 };
