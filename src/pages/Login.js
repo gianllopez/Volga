7e97e31c14cbe1ca828f0d6d1794cea233c3b21d
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-25 02:01:25 -0500

    Login: modificados los assets y algunos textos, a fetchear.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,68 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Input } from './components/';
+import { Input, ButtonLoader } from './components/';
 import loginhero from '../assets/Login/login-hero.svg';
 import './styles/Login.css';
+import { noBlankValidator } from '../utils/validators';
 
 class Login extends Component {
+
+   state = {
+      data: {
+         username: '',
+         password: ''
+      },
+      loading: false,
+      errors: {}
+   };
+
+   changeHandler = ({ target }) => {
+      this.setState({
+         data: {
+            ...this.state.data,
+            [target.name]: target.value
+         }
+      });
+   };
+
+   submitHandler = event => {
+      event.preventDefault();
+      let { isValid, errors } = noBlankValidator(this.state.data);
+      if (isValid) {
+         console.log('Make fetch');
+      } else {
+         this.setState({ errors });
+      };
+   };
+
    render() {
       return (
-         <form id="login-form">
+         <form id="login-form" onSubmit={this.submitHandler}>
             <div id="login-header">
                <img src={loginhero} alt="login-hero" />
                <div id="header-text">
                   <h1>Inicia sesión</h1>
-                  <p>Mantente al día con tu tienda</p>
-            </div>
+                  <p>Mantente al día con tus productos</p>
+               </div>
             </div>
             <div id="login-entries">
                <Input
-                  label="Tienda o Email"
-                  name="shoporemail"
-                  type="email"
+                  label="Nombre de usuario"
+                  name="username"
+                  onChange={this.changeHandler}
+                  errors={this.state.errors}
                />
                <Input
                   label="Contraseña"
                   name="password"
                   type="password"
+                  onChange={this.changeHandler}
+                  errors={this.state.errors}
                />
-               {/* Button & Loader Component */}
+               <ButtonLoader isloading={this.state.loading} />
                <p>
-                  ¿No has registrado tu tienda?<br/>
-                  <Link to="/logup">
-                     Regístrala
-                  </Link>
+                  ¿No te has registrado?<br />
+                  <Link to="/logup">Regístrate</Link>
                </p>
             </div>
          </form>
