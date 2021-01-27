daca6a00465007fdbf5fb756714e767694282145
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-27 17:43:14 -0500

    All: solucionado el error del 404, hay que bloquear las pages de /login and /logup cuando auth = true.

--- src/index.js
+++ src/index.js
@@ -27,20 +27,19 @@ ReactDOM.render(
          <ProtectedRoute exact path="/:username/tags" component={UserTags} /> {/* Ready, por revisar si hay código que resumir... */}
          <ProtectedRoute exact path="/:username/profile-picture" component={UserProfilePicture} /> {/* Ready, por revisar si hay código que resumir... */}
          <MainLayout nofooter={['/login']}>
-            <Route exact path="/" component={Home} />
-            {!isAuthenticated ? (
-               <Fragment>
-                  <Route exact path="/logup" component={Logup} /> {/* Ready, por revisar si hay código que resumir... */}
-                  <Route exact path="/login" component={Login} />
-               </Fragment>) : <Redirect to="/" />}
-            <Route exact path="/users/:username" component={UserProfile} />
-            <Route exact path="/:username/catalog/:product" component={ProductPage} />
-            <ProtectedRoute exact path="/:username/products/new" component={PostProduct} />
-            <Route exact path="/:username/opinions" component={ClientsOpinions} /> {/* Ready, por revisar si hay código que resumir... */}
-            <ProtectedRoute exact path="/:username/opinions/new" component={NewOpinion} /> {/* Ready, por revisar si hay código que resumir... */}
-            <Route exact path="/:shop/contact" component={ShopContact} />
-            <Route exact path="/search/results" component={SearchResults} />
-            <Route component={NotFound} />
+            <Switch>
+               <Route exact path="/" component={Home} />
+               <Route exact path="/logup" component={Logup} /> {/* Ready, por revisar si hay código que resumir... */}
+               <Route exact path="/login" component={Login} />
+               <Route exact path="/users/:username" component={UserProfile} />
+               <Route exact path="/:username/catalog/:product" component={ProductPage} />
+               <ProtectedRoute exact path="/:username/products/new" component={PostProduct} />
+               <Route exact path="/:username/opinions" component={ClientsOpinions} /> {/* Ready, por revisar si hay código que resumir... */}
+               <ProtectedRoute exact path="/:username/opinions/new" component={NewOpinion} /> {/* Ready, por revisar si hay código que resumir... */}
+               <Route exact path="/:shop/contact" component={ShopContact} />
+               <Route exact path="/search/results" component={SearchResults} />
+               <Route component={NotFound} />
+            </Switch>
          </MainLayout>
       </Switch>
    </BrowserRouter >, document.getElementById('root'));
