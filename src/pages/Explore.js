4b5d451a00d3bc14740a4f8b6d94bbb0a11eedfa
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-02-09 00:25:01 -0500

    TagsSelector: ya togglea la clase en los <p>.

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -3,11 +3,28 @@ import { ButtonLoader, TagsSelector } from './components';
 import './styles/Explore.css';
 
 class Explore extends Component {
+
+   state = { querytags: [] };
+
+   selectHandler = target => {
+      let tag = target.innerText,
+      { querytags } = this.state;
+      if (querytags.includes(tag)) {
+         querytags.splice(querytags.indexOf(tag), 1);
+      } else {
+         querytags.push(tag);
+      };
+      this.setState({ querytags });
+   };
+
    render() {
       return (
          <div id="explore-page">
             <h1>Etiquetas a explorar:</h1>
-            <TagsSelector/>
+            <TagsSelector
+               onSelect={this.selectHandler}
+               selectedTags={this.state.querytags}
+            />
             <ButtonLoader/>
          </div>
       );
--- src/pages/components/Explore/TagsSelector.js
+++ src/pages/components/Explore/TagsSelector.js
@@ -3,18 +3,27 @@ import { tagsProps } from '../../../assets';
 import './TagsSelector.css';
 
 class TagsSelector extends Component {
+
+
+   clickHandler = ({ target }) => {
+      let { selectedTags, onSelect } = this.props;
+      target.classList.toggle('selected-tag');
+      onSelect(target);
+   };
+
    render() {
       let tags = Object.keys(tagsProps).sort((a, b) => a.length - b.length);
       return (
          <div id="tags-selector">
             {tags.map((tag, index) => (
-               <p key={index}>
+               <p key={index} onClick={this.clickHandler}>
                   {tag}
                </p>
             ))}
          </div>
       );
    };
+
 };
 
 export default TagsSelector;
