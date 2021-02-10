bff31b85477eadf972a46f6713402bf429176bc0
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-02-10 16:45:13 -0500

    FavoritesProducts: ya obtiene los datos del backend.

--- src/pages/FavoritesProducts.js
+++ src/pages/FavoritesProducts.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import api from '../utils/api';
+import './styles/FavoritesProducts.css';
 
 class FavoritesProducts extends Component {
+   
    render() {
-      return (<div></div>);
+      return (
+         <div id="favorites-products">
+            <h2>Estos son tus productos favoritos:</h2>
+         </div>
+      );
+   };
+
+   componentDidMount() {
+      api.get('/get-data/favorites-products')
+      // .then(())
    };
 };
 
